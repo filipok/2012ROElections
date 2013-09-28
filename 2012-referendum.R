f5a9e866b1ad432fde205fed8b5c677607890628
filipok <filip.gadiuta@gmail.com>
2013-09-28 21:14:37 +0300

    massive code reorganisation, bugs expected, not done yet

--- 2012-referendum.R
+++ 2012-referendum.R
@@ -1,18 +1,5 @@
-################################################################################
-ref2012Sv = read.csv("2012referendum/2. rezultate/sv_date.csv", sep = ";", 
-                           stringsAsFactors = FALSE)
-################################################################################
-colnames(ref2012Sv)[1] = "JUD"
-colnames(ref2012Sv)[2] = "DEN_JUD"
-colnames(ref2012Sv)[6] = "SV"
-colnames(ref2012Sv)[5] = "DEN_CIRC_R"
-ref2012Sv$DEN_JUD = gsub("BACĂU", "BACAU", ref2012Sv$DEN_JUD) #spelling
-ref2012Sv$DEN_JUD = gsub("BOTOSANI", "BOTOŞANI", ref2012Sv$DEN_JUD) #spelling
-ref2012Sv$DEN_JUD = gsub("BRĂILA", "BRAILA", ref2012Sv$DEN_JUD) #spelling
-ref2012Sv$DEN_JUD = gsub("CĂLĂRAŞI", "CALARAŞI", ref2012Sv$DEN_JUD) #spelling
-ref2012Sv$DEN_JUD = gsub("CONSTANTA", "CONSTANŢA", ref2012Sv$DEN_JUD) #spelling
-ref2012Sv$DEN_JUD = gsub("SĂLAJ", "SALAJ", ref2012Sv$DEN_JUD) #spelling
-ref2012Sv$DEN_JUD = gsub("Municipiul Bucuresti", "MUNICIPIUL BUCUREŞTI", ref2012Sv$DEN_JUD) #spelling
+source("2012-referendum-voturi.R")
+
 #am siruta la referendum şi am şi la sv.xlsx în locale 2012
 #
 # cumulare la nivel de CIRC/SIRUTA (similar funcţiei CumulatCirc din locale2012)
---
+++ examples-2012locale.R
@@ -0,0 +1,38 @@
+#Source first the files below:
+source("get_data.R")
+source("2012-locale.R")
+source("2012-locale-functions.R")
+
+
+#Exemple de utilizare a funcţiilor#
+
+#pe judeţ pentru cele unde avem CIRC = 0
+cjuAbsJud = AbsolutJudet(CJU)
+pcjAbsJud = AbsolutJudet(PCJ)
+#pe judeţ pentru cele unde nu avem CIRC = 0, cu agregare independenţi
+cloCumJud = CumulatJudet(CLO) #cumulare pe bază de circumscripţie
+priCumJud = CumulatJudet(PRI) #cumulare pe bază de circumscripţie
+
+#pe circumscripţie -> CIRC != 0 & SV == 0
+#pentru cele cu date la nivel de circumscripţie
+cloAbsCirc = AbsolutCirc(CLO, TRUE)
+priAbsCirc = AbsolutCirc(PRI, TRUE)
+#pentru cele fără date la nivel de circumscripţie
+cjuCumCirc = CumulatCirc(CJU) #cumulare pe bază de secţie de votare
+pcjCumCirc = CumulatCirc(PCJ) #cumulare pe bază de secţie de votare
+
+#pe secţie de votare - pentru PRI şi CLO avem date doar pentru Bucureşti
+pcjAbsSv = AbsolutSv(PCJ)
+cjuAbsSv = AbsolutSv(CJU)
+cloAbsSvBuc = AbsolutSv(CLO, TRUE)
+priAbsSvBuc = AbsolutSv(PRI, TRUE)
+
+Procente(AbsolutJudet(CJU, TRUE)) #exemplu utilizare funcţie Procente
+SimpLocale2012(AbsolutSv(CJU)) #exemplu utilizare funcţie SimpLocale2012
+Procente(SimpLocale2012(AbsolutSv(CJU, TRUE))) #combinare funcţii
+#
+#Rezultate primării la nivel de circumscripţie TUR 1 + TUR 2
+priCirc = Procente(SimpLocale2012(AbsolutCirc(PRI, TRUE)))
+priCirc2 = TUR2[TUR2$TIPPV == 1,]
+priCirc2 = Procente(SimpLocale2012(AbsolutCirc(priCirc2, TRUE)))
+priCircAll = rbind(priCirc, priCirc2)
